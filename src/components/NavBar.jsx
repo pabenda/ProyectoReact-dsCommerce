a075dda21705d47c0c787f6a18fcad7501a2897f
Pablo D. Bendayan <pablodb@gmail.com>
2023-02-04 12:40:14 -0300

    se agrega menú desplegable

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import CartWidget from './CartWidget'
+import { Select } from '@chakra-ui/react'
 
 const NavBar = () => {
     return (
         <div>
             <h3>dsCommerce - Materiales para impresión 3d</h3>
-            <ul>
-                <li>Inicio</li>
-                <li>Acerca de</li>
-                <li>Contacto</li>
-            </ul>
-            <CartWidget/>
+            <Select placeholder='Elija una opción'>
+                <option value='option1'>Quienes Somos</option>
+                <option value='option2'>Productos</option>
+                <option value='option3'>Novedades</option>
+            </Select>
+            <CartWidget />
         </div>
     )
 }
