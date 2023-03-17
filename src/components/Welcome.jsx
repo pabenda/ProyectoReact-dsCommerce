6cf1e677da2acb1390f438b8905a35bf9b307593
Pablo D. Bendayan <pablodb@gmail.com>
2023-03-17 08:35:58 -0300

    se agrega carrousel a pagina de inicio

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import "../main.css";
 
 const Welcome = () => {
+  const [currentImage, setCurrentImage] = useState('./src/assets/imagenWelcome.jpg');
+  const imageList = ['./src/assets/imagenWelcome.jpg', './src/assets/imagen2.jpg', './src/assets/imagen3.jpg'];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeImage();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentImage]);
+
+  const changeImage = () => {
+    const currentIndex = imageList.indexOf(currentImage);
+    if (currentIndex === imageList.length - 1) {
+      setCurrentImage(imageList[0]);
+    } else {
+      setCurrentImage(imageList[currentIndex + 1]);
+    }
+  };
+
   return (
     <div className="imagenWelcome">
-           <img src="./src/assets/imagenWelcome.jpg" alt="impresion3d" className='imgWelc'/>
+      <img src={currentImage} alt="impresion3d" className='imgWelc'/>
     </div>
   );
 };
